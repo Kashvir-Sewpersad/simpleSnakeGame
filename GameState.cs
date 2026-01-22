4b37fee521e3ceab1a76c414a5902f810cc1c502
kashc <kashc@THE_BEAST>
2026-01-22 18:20:34 +0200

    ADDED GAME STATE , SNAKE AND EMPTY POSITION

---
+++ GameState.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleSnakeGame
+{
+    public  class GameState
+    {
+        public int Rows { get; }
+        public int Cols { get; }
+
+        public GridValue[,] Grid { get; }
+        public Direction Dir { get; private set; }
+        public int Score { get; private  set; }
+        public bool  GameOver { get; private set; }
+
+        private readonly  LinkedList<Position > snakePosiition = new LinkedList<Position>();
+
+        private readonly Random random = new Random();
+
+
+
+
+        public GameState(int rows, int cols) { 
+            Rows = rows;
+            Cols = cols;
+            Grid = new GridValue[rows,cols];
+            Dir = Direction.Right;
+
+
+
+            AddSnake();
+        
+        }
+
+
+        private void AddSnake() {
+
+            int r = Rows / 2;
+            for (int c = 1; c <= 3; c++)
+            {
+                Grid[r, c] = GridValue.Snake;
+
+                snakePosiition.AddFirst(new Position(r, c)); 
+            }
+        
+        
+        
+        }
+
+
+
+        private IEnumerable<Position> EmptyPositions() {
+
+            for (int r = 0; r < Rows; r++) {
+                for (int c = 0; c < Cols; c++) {
+                    if (Grid[r, c] == GridValue.Empty)
+                    { 
+                        yield return new Position (r, c);
+                    }
+                
+                }
+            
+            
+            }
+        
+        }
+
+
+
+
+
+
+    }
+}
