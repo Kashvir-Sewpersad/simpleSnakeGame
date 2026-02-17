ee81d1bf1e7aca24bfc23538e724030754f470dd
kashc <kashc@THE_BEAST>
2026-02-17 14:03:17 +0200

    restarted the gamestate class

--- GameState.cs
+++ GameState.cs
@@ -4,74 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
+/// we have a issue here with the spelling of positions ---..> positiions etc. note this is a failure point 
+/// 19min in tut ---- > 22 min 
+
 namespace simpleSnakeGame
 {
-    public  class GameState
+    public class GameState
     {
         public int Rows { get; }
         public int Cols { get; }
-
         public GridValue[,] Grid { get; }
         public Direction Dir { get; private set; }
-        public int Score { get; private  set; }
-        public bool  GameOver { get; private set; }
+        public int  Score{get; private set;}
 
-        private readonly  LinkedList<Position > snakePosiition = new LinkedList<Position>();
+        public bool GameOver { get; private set; }
 
+        private readonly LinkedList<Position> snakePosition = new LinkedList<Position>();
         private readonly Random random = new Random();
-
-
-
-
-        public GameState(int rows, int cols) { 
-            Rows = rows;
-            Cols = cols;
-            Grid = new GridValue[rows,cols];
-            Dir = Direction.Right;
-
-
-
-            AddSnake();
-        
-        }
-
-
-        private void AddSnake() {
-
-            int r = Rows / 2;
-            for (int c = 1; c <= 3; c++)
-            {
-                Grid[r, c] = GridValue.Snake;
-
-                snakePosiition.AddFirst(new Position(r, c)); 
-            }
-        
-        
-        
-        }
-
-
-
-        private IEnumerable<Position> EmptyPositions() {
-
-            for (int r = 0; r < Rows; r++) {
-                for (int c = 0; c < Cols; c++) {
-                    if (Grid[r, c] == GridValue.Empty)
-                    { 
-                        yield return new Position (r, c);
-                    }
-                
-                }
-            
-            
-            }
-        
-        }
-
-
-
-
-
-
     }
 }
