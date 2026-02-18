3852a91732fb57cf7bd938709702b35a2b484737
kashc <kashc@THE_BEAST>
2026-02-18 20:46:14 +0200

    updated the game state class with nnew methods and additional functionality

--- GameState.cs
+++ GameState.cs
@@ -23,5 +23,162 @@ namespace simpleSnakeGame
 
         private readonly LinkedList<Position> snakePosition = new LinkedList<Position>();
         private readonly Random random = new Random();
+
+
+        public GameState(int rows, int cols ) 
+        {
+                Rows = rows;
+                Cols = cols;
+                Grid = new GridValue[rows, cols];
+                Dir = Direction.Right;
+
+                AddSnake();
+            AddFood();
+        }
+
+        private void AddSnake() 
+        {
+            int r = Rows / 2;
+
+            for (int c = 1; c <= 3; c++)
+            {
+                Grid[r, c] = GridValue.Snake;
+                snakePosition.AddFirst(new Position(r, c));
+            }
+        }
+
+        private IEnumerable<Position> EmptyPositions()
+        {
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Cols; c++)
+                {
+                    if (Grid[r, c] == GridValue.Empty)
+                    {
+                        yield return new Position(r, c);
+                    }
+                }
+            }
+        }
+
+        private void AddFood() 
+        {
+            List<Position> empty = new List<Position>(EmptyPositions());
+
+            if (empty.Count == 0)
+            {
+                return;
+            }
+
+            Position pos = empty[random.Next(empty.Count)];
+            Grid[pos.Row,pos.Col] = GridValue.Food;
+        }
+
+
+        public Position HeadPosition()
+        {
+            return snakePosition.First.Value;
+        }
+
+        public Position TailPosition()
+        { 
+            return snakePosition.Last.Value;
+        }
+
+        public IEnumerable<Position> SnakePositions()
+        {
+            return snakePositions;
+        }
+
+        private void AddHead(Position pos)
+        {
+            snakePositions.AddFirst(pos);
+            Grid[pos.Row, pos.Col] = GridValue.Snake;
+
+        }
+
+        private void RemoveTail()
+        {
+            Position tail = snakePositions.Last.Value;
+            Grid[tail.Row, tail.Col] = GridValue.Empty;
+            snakePositions.RemoveLast();
+        
+        
+        }
+        public void ChangeDirection(Direction dir )
+        {
+
+            Dir = dir; 
+        }
+
+        private bool OutsideGrid(Position pos)
+        { 
+        
+        return pos.Row<0 || pos.Row>= Rows|| pos.Col <0 || pos.Col>= Cols;
+        }
+
+        private GridValue WillHit(Position newHeadPos)
+
+        {
+
+            if (OutsideGrid(newHeadPos))
+                {
+                    return GridValue.Outside;
+            
+                }
+            if (newHeadPos == TailPosition())
+                {
+                    return GridValue.Empty;
+            
+                }
+
+            return Grid[newHeadPos.Row, newHeadPos.Col];
+        }
+
+
+        public void Move() 
+        {
+            Position newHeadPos = HeadPosition().Translate(Dir); 
+            GridValue hit = WillHit(newHeadPos);
+
+            if (hit == GridValue.Outside || hit == GridValue.Snake)
+            {
+                GameOver = true;
+
+            }
+            else if (hit == GridValue.Empty)
+            {
+                RemoveTail();
+                AddHead(newHeadPos);
+
+            }
+            else if (hit == GridValue.Food)
+            {
+
+                AddHead(newHeadPos);
+                Score++;
+                AddFood();
+            
+            
+            
+            
+            }
+        
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        }
     }
-}
