ab45ffcb4635eacd5310bd4f81eefc143d4996e3
kashc <kashc@THE_BEAST>
2026-02-24 14:13:09 +0200

    Commented and added headers to the gridValue class

--- GridValue.cs
+++ GridValue.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿
+//*************************************** START OF FILE ******************************//
+
+//---------------- START OF IMPORTS -----------------//
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//---------------- END OF IMPORTS -------------------//
 namespace simpleSnakeGame
 {
-   public enum GridValue
+    /// <summary>
+    ///  This enum defines the four states a cell can be in.
+    /// Empty, occupied by the snake, containing food or outside the playable area 
+    /// </summary>
+    public enum GridValue
     {
         Empty, Snake, Food, Outside 
     }
 }
+//****************************************** END OF FILE ***************************// 
